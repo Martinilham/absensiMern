fab285a18aba84f714a2bcaf86dc140b6d9d6df0
Martin <ilhammartin2@gmail.com>
2024-01-30 22:38:57 +0700

    .

--- backend/models/absensi.js
+++ backend/models/absensi.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
 
 const Absensi = mongoose.Schema(
-    {    
+    {   
+        nama:{
+            type:Date,
+            required:true
+        },
+        kelas:{
+            type:Date,
+            required:true
+        }, 
         hari:{
             type:Date,
             required:true
