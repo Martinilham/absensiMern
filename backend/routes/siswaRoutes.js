91916ef09481f9c7d615460e30b8b5f65ca52702
Martin <ilhammartin2@gmail.com>
2024-01-31 22:03:34 +0700

    .

--- backend/routes/siswaRoutes.js
+++ backend/routes/siswaRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import {
-    getUsers,
-    getUserById,
-    saveUser,
-    updateUser,
-    deleteUser
+    getSiswa,
+    getSiswaByNis,
+    saveSiswa,
+    updateSiswa,
+    deleteSiswa
 } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
-router.get('/users',getUsers);
-router.get('/users/:id',getUserById);
-router.post('/users',saveUser);
-router.patch('/users/:id',updateUser);
-router.delete('/users/:id',deleteUser)
+router.get('/users',getSiswa);
+router.get('/users/:id',getSiswaByNis);
+router.post('/users',saveSiswa);
+router.patch('/users/:id',updateSiswa);
+router.delete('/users/:id',deleteSiswa)
 
 export default router
